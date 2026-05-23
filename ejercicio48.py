c7fc6271183960194c12ca2cfb0ce7b418f181c1
aliagaeyzaguirrej-lgtm <aliagaeyzaguirrej@gmail.com>
2026-05-23 15:34:08 -0400

    mi primer commit

---
+++ ejercicio48.py
@@ -0,0 +1,21 @@
+num1 = float(input("ingrese un numero"))
+num2 = float(input("ingrese el siguiente numero"))
+operacion= input("ingrese la operacion que quiera realizar +, -, *, /")
+resultado = 0
+if operacion == "+":
+    resultado = num1+ num2
+    print(f"el resultado es {resultado}")
+elif operacion == "-":
+    resultado = num1 - num2
+    print(f"el resuultado es {resultado}")
+elif operacion == "*":
+    resultado = num1 * num2
+    print(f"el resultado es {resultado}")
+elif operacion == "/":
+    if num2 == 0:
+        print("Error no se puede dividir entre 0")
+    else:
+        resultado = num1 / num2
+        print(f"el resuktado es {resultado}")
+else:
+    print("error introdusca un valor valido")
