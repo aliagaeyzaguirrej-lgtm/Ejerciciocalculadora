a95790d3dbaf4cb3c15d12cce7994b0f9b0e6756
Tzortheos2929v <duicornista@gmail.com>
2026-06-18 14:47:56 -0400

    Trabajo de Luis

--- ejercicio 16.py
+++ ejercicio 16.py
@@ -1,11 +1,12 @@
 nombre="esternocleidomastoideo"
-busqueda ="z"
+busqueda ="e"
 encontrado=False
 for letra in nombre:
     if letra== busqueda:
+        
         encontrado= True
         break
 if encontrado:
-    print(f"la letra'{busqueda}' si esta en la palabra.")
+    print(f"la letra'{busqueda}' si esta en la palabra. y ")
 else:
     print(f"no se encontro la letra.")    
